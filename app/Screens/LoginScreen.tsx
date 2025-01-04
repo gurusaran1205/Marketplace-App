5f8bbbadccc30eec4b19e82d09ff87b543288b76
Gurusaran A B <gurusaranofficial1@gmail.com>
2025-01-05 00:32:07 +0530

    second day

--- app/Screens/LoginScreen.tsx
+++ app/Screens/LoginScreen.tsx
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { Image } from 'react-native'
+import * as WebBrowser from 'expo-web-browser';
+import { useWarmUpBrowser } from '@/hooks/warmUpBrowser';
+import { useOAuth } from '@clerk/clerk-expo';
+import { ClerkProvider } from '@clerk/clerk-expo';
 
+WebBrowser.maybeCompleteAuthSession();
 const LoginScreen = () => {
+
+  useWarmUpBrowser();
+
+  const {startOAuthFlow} = useOAuth({strategy: "oauth_google"});
+
+  const onPress = React.useCallback(async () => {
+    try{
+      const { createdSessionId, signIn, signUp, setActive} = 
+        await startOAuthFlow();
+
+      if (createdSessionId){
+        setActive({ session: createdSessionId});
+      } else {
+        console.warn('No session was created.')
+      }
+      } catch (err){
+        console.error("OAuth error", err);
+      }
+    }, []);
+
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <Image source={require('../../assets/images/login2.jpg')}
+        className='w-full h-[400px] object-cover'/>
+        <View className='p-10 bg-white mt-[-20px] rounded-t-3xl shadow-md'>
+          <Text className='text-[30px] font-bold'>Community Marketplace</Text>
+          <Text className='text-[18px] text-slate-500 mt-6'>Buy Sell Marketplace where you can sell old items and make real money</Text>
+          <TouchableOpacity onPress={onPress} className='p-4 bg-blue-700 rounded-full mt-20'>
+            <Text className='text-white text-center text-[18px]'> Get Started</Text>
+          </TouchableOpacity>
+        </View>
     </View>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,24 @@ import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "react-native";
 import React from "react";
 import LoginScreen from "./Screens/LoginScreen";
-
+import {ClerkProvider, SignedIn, SignedOut} from '@clerk/clerk-expo';
 
 export default function Page() {
   return (
+    <ClerkProvider publishableKey="pk_test_c29saWQtbW9jY2FzaW4tNzUuY2xlcmsuYWNjb3VudHMuZGV2JA">
     <View className="flex-1">
       <StatusBar />
-      <LoginScreen/>
+      
+      <SignedIn>
+        <Text>You are Signed In</Text>
+      </SignedIn>
+      <SignedOut>
+        <LoginScreen/>
+      </SignedOut>
+
+
       </View>
+      </ClerkProvider>
   );
 }
 
